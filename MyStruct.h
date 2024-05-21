780aff583f1f69ef7449d65aedc92623925f0536
Pranav Rao <pranavanrao@gmail.com>
2024-05-21 12:54:13 +0530

    Add separate header file for the structure

---
+++ MyStruct.h
@@ -0,0 +1,12 @@
+#ifndef MYSTRUCT_H
+#define MYSTRUCT_H
+
+struct MyStruct {
+    int a = -15;
+    double b = 3.14159;
+    float c = 1.123f;
+    char d = 'F';
+    bool e = true;
+};
+
+#endif // MYSTRUCT_H
--- main.cpp
+++ main.cpp
@@ -1,35 +1,27 @@
+#include "MyStruct.h"
+
 #include <iostream>
 
 using namespace std;
 
-struct MyStruct {
-    int a = -15;
-    double b = 3.14159;
-    float c = 1.123f;
-    char d = 'F';
-    bool e = true;
-};
-
 int main()
 {
-    // MyStruct structure;
+    MyStruct structure;
 
-    // cout << "Integer : " << structure.a << " With size : " << sizeof(structure.a) << endl;
-    // cout << "Double : " << structure.b << " With size : " << sizeof(structure.b) << endl;
-    // cout << "Float : " << structure.c << " With size : " << sizeof(structure.c) << endl;
-    // cout << "Character : " << structure.d << " With size : " << sizeof(structure.d) << endl;
-    // cout << "Boolean : " << (structure.e ? "True" : "False") << " With size : " << sizeof(structure.e) << endl;
-
-    MyStruct *ptr = nullptr;
+    cout << "Integer : " << structure.a << " With size : " << sizeof(structure.a) << endl;
+    cout << "Double : " << structure.b << " With size : " << sizeof(structure.b) << endl;
+    cout << "Float : " << structure.c << " With size : " << sizeof(structure.c) << endl;
+    cout << "Character : " << structure.d << " With size : " << sizeof(structure.d) << endl;
+    cout << "Boolean : " << (structure.e ? "True" : "False") << " With size : " << sizeof(structure.e) << endl;
 
     // Size of the structure
-    cout << "Size of the structure : " << sizeof(*ptr) << " Bytes" << endl;
+    cout << "Size of the structure : " << sizeof(structure) << " Bytes" << endl;
 
     // Address of the structure
-    cout << "Address of thr structure : " << ptr << endl;
+    cout << "Address of thr structure : " << &structure << endl;
 
     // Type caste the structure address into char*
-    char* sturct_ptr = (char*)(ptr);
+    char* sturct_ptr = (char*)(&structure);
     cout << "Address of the structure as (char*) : " << &sturct_ptr << endl;
     return 0;
 }
